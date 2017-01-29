fb1679233b12857667f09529abf7e5ba6407654b
Chiemeka Ihebom <emekavilla@gmail.com>
2017-01-29 12:23:59 -0500

    adding auth routes

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -6,3 +6,29 @@ const models = require('../db/models/index');
 function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
+
+//If the user is logged in, they'll be redirected to their page and
+//and we go on to the next middleware.
+function loginRedirect(req, res, next) {
+  if (req.user) return res.status(401).json(
+    { status: 'You are already logged in' }
+  );
+
+  return next();
+}
+//information entered by user is being stored in this function
+function createUser(req, res) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  return models.User.create({
+    username: req.body.username,
+    password: hash,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dob: req.body.dob
+  }).then(() => {
+    res.redirect('/');
+  });
+}
---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const authHelpers = require('../auth/auth-helpers');
+const passport = require('../auth/local');
+
+router.get('/register', authHelpers.loginRedirect, (req, res)=> {
+  res.render('auth/register');
+});
+//creating new user account after registration
+router.post('/register', (req, res, next)  => {
+  return authHelpers.createUser(req, res)
+  .then((response) => {
+    console.log('registration successful');
+  })
+  .catch((err) => { res.status(500).json({ status: 'error' }); });
+});
+//directing users to the login page
+router.get('/login', authHelpers.loginRedirect, (req, res)=> {
+  res.render('auth/login');
+});
+//grabs the login info and redirects to user page if logged in or login page if not
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+  })
+);
+//user logs out
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+
+module.exports = router;
